5c1a6ae311b1f69a6649d8c5dc78ebc8f4a7f513
Nick <shiroaimai@gmail.com>
2021-09-22 19:55:12 +0200

    Create a version of 3d hash space Add another separate job in order to handle shapes positions and normals separately Vectorize jobs with simd instructions

--- Assets/Scripts/SmallXXHash.cs
+++ Assets/Scripts/SmallXXHash.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 /**
@@ -27,6 +28,9 @@ public readonly struct SmallXXHash {
         return avalanche;
     }
     
+    public static implicit operator SmallXXHash4 (SmallXXHash hash) =>
+        new SmallXXHash4(hash.accumulator);
+    
     public SmallXXHash(uint accumulator)
     {
         this.accumulator = accumulator;
@@ -39,4 +43,37 @@ public readonly struct SmallXXHash {
     public SmallXXHash Eat(int data) => RotateLeft(accumulator + (uint) data * primeC, 17) * primeD;
 
     static uint RotateLeft(uint data, int steps) => (data << steps) | (data >> 32 - steps); 
+}
+
+public readonly struct SmallXXHash4 {
+
+    const uint primeB = 0b10000101111010111100101001110111;
+    const uint primeC = 0b11000010101100101010111000111101;
+    const uint primeD = 0b00100111110101001110101100101111;
+    const uint primeE = 0b00010110010101100110011110110001;
+
+    private readonly uint4 accumulator;
+
+    public static implicit operator uint4(SmallXXHash4 hash)
+    {
+        uint4 avalanche = hash.accumulator;
+        avalanche ^= avalanche >> 15;
+        avalanche *= primeB;
+        avalanche ^= avalanche >> 13;
+        avalanche *= primeC;
+        avalanche ^= avalanche >> 16;
+        return avalanche;
+    }
+    
+    public SmallXXHash4(uint4 accumulator)
+    {
+        this.accumulator = accumulator;
+    }
+
+    public static SmallXXHash4 Seed(int4 seed) => (uint4) seed + primeE;
+    public static implicit operator SmallXXHash4(uint4 accumulator) => new SmallXXHash4(accumulator);
+
+    public SmallXXHash4 Eat(int4 data) => RotateLeft(accumulator + (uint4) data * primeC, 17) * primeD;
+
+    static uint4 RotateLeft(uint4 data, int steps) => (data << steps) | (data >> 32 - steps); 
 }
